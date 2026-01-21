71562e7da9d12946c58e7aebd6ad43a850aee265
Drishti Gupta <thedrishtigupta@gmail.com>
2026-01-21 21:02:26 +0530

    LCA solution in BT

---
+++ 7_Trees/235_LCA_BST_LC.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+
+class Solution1 {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root) return NULL;
+
+        if(root->val == p->val || root->val == q->val) return root;
+
+        TreeNode* leftLCA = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root->right, p, q);
+
+        if(leftLCA && rightLCA) return root;
+        else if (leftLCA) return leftLCA;
+        else return rightLCA;
+    }
+};
